6a15dde067db33abf5a0f4d28d70a170578b7474
Louies-Jhony <louies.jhony@ibm.com>
2025-02-06 14:49:37 +0530

    Baremetal Module Update

--- main.tf
+++ main.tf
@@ -20,10 +20,17 @@ locals {
 }
 
 # Dedicated Host Group
+locals {
+  unique_dedicated_hosts = {
+    for i, group in var.dedicated_hosts :
+    "${group.host_group_name}-${i}" => group
+  }
+}
+
 resource "ibm_is_dedicated_host_group" "dh_group" {
   for_each = {
-    for group in var.dedicated_hosts :
-    group.host_group_name => group
+    for key, group in local.unique_dedicated_hosts :
+    key => group
     if group.existing_host_group == false
   }
 
@@ -60,10 +67,11 @@ locals {
   flattened_hosts = flatten([
     for group in var.dedicated_hosts : [
       for host in group.dedicated_host : {
-        key               = "${group.host_group_name}-${host.name}" # Ensure uniqueness
-        name              = host.name
-        profile           = host.profile
-        host_group_id     = group.existing_host_group ? data.ibm_is_dedicated_host_group.existing_dh_group[group.host_group_name].id : ibm_is_dedicated_host_group.dh_group[group.host_group_name].id
+        key           = "${group.host_group_name}-${host.name}" # Ensure uniqueness
+        name          = host.name
+        profile       = host.profile
+        host_group_id = group.existing_host_group ? data.ibm_is_dedicated_host_group.existing_dh_group["${group.host_group_name}-${host.name}"].id : ibm_is_dedicated_host_group.dh_group["${group.host_group_name}-${index(var.dedicated_hosts, group)}"].id
+        #        host_group_id     = group.existing_host_group ? data.ibm_is_dedicated_host_group.existing_dh_group[group.host_group_name].id : ibm_is_dedicated_host_group.dh_group[group.host_group_name].id
         resource_group_id = group.resource_group_id
         access_tags       = host.access_tags
       }
