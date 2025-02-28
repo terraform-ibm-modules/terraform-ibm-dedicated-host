7075cd19577c39ca77867c111e50a263c97488a0
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-02-28 21:35:26 +0000

    chore(deps): update terraform terraform-ibm-modules/landing-zone-vpc/ibm to v7.20.1

--- examples/upgrade/main.tf
+++ examples/upgrade/main.tf
@@ -55,7 +55,7 @@ resource "ibm_is_ssh_key" "ssh_key" {
 
 module "slz_vpc" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "7.19.1"
+  version           = "7.20.1"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
