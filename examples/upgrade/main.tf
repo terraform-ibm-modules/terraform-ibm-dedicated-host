2712d569eba5e5aa674080d737bf1b46822c859d
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-09-12 20:26:33 +0000

    chore(deps): update dependencies

--- examples/upgrade/main.tf
+++ examples/upgrade/main.tf
@@ -55,7 +55,7 @@ resource "ibm_is_ssh_key" "ssh_key" {
 
 module "slz_vpc" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "8.2.2"
+  version           = "8.3.0"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
@@ -73,7 +73,7 @@ data "ibm_is_image" "slz_vsi_image" {
 
 module "slz_vsi" {
   source                = "terraform-ibm-modules/landing-zone-vsi/ibm"
-  version               = "5.5.4"
+  version               = "5.6.0"
   resource_group_id     = module.resource_group.resource_group_id
   image_id              = data.ibm_is_image.slz_vsi_image.id
   create_security_group = false
